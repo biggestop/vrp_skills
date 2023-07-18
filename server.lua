9dd8e6c01c6a13159c7e5e5cc993581607666dd1
$legend <107737400+trenchesLegend@users.noreply.github.com>
2023-07-18 12:55:53 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,50 +1,40 @@
 local uSkills = {}
 local MinExperience = 100
+local MaxSkillLevel = 3
 
-function SkillLevelUp(user_id,skill)
-    if user_id then
-        local UserLevel = vRP.GetPlayerSkill(user_id,skill)
-        if UserLevel == 0 then
-            uSkills[user_id][skill] = {Nivel = 1 , xp = 0}
-        else
-            if UserLevel == 1 then
-                uSkills[user_id][skill] = {Nivel = 2 , xp = 0}
-            else
-                if UserLevel == 2 then
-                    uSkills[user_id][skill] = {Nivel = 3 , xp = 0}
-                end
+function SkillLevelUp(user_id, skill)
+    if user_id and uSkills[user_id] and uSkills[user_id][skill] then
+        local UserLevel = uSkills[user_id][skill].Nivel
+        if UserLevel < MaxSkillLevel then
+            uSkills[user_id][skill].xp = 0
+            uSkills[user_id][skill].Nivel = UserLevel + 1
+        end
+    end
+end
+
+function vRP.GiveSkillExperience(user_id, skill, exp)
+    if user_id and uSkills[user_id] and uSkills[user_id][skill] then
+        local level = uSkills[user_id][skill].Nivel
+        if level < MaxSkillLevel then
+            uSkills[user_id][skill].xp = uSkills[user_id][skill].xp + exp
+            if uSkills[user_id][skill].xp >= MinExperience then
+                SkillLevelUp(user_id, skill)
             end
         end
     end
 end
 
-function vRP.GiveSkillExperience(user_id,skill,exp)
-    if user_id then
-        local level = vRP.GetPlayerSkill(user_id,skill)
-        if level < 3 then
-            if uSkills[user_id][skill] then
-                uSkills[user_id][skill].xp = uSkills[user_id][skill].xp + exp
-                if uSkills[user_id][skill].xp >= MinExperience then
-                    SkillLevelUp(user_id,skill)
-                end
-            end
-        end
-    end
-end
-
-function vRP.GetPlayerSkill(user_id,skill)
-    if user_id then
-        if not uSkills[user_id][skill] then
-            uSkills[user_id][skill] = {Nivel = 0 , xp = 0}
-        end
+function vRP.GetPlayerSkill(user_id, skill)
+    if user_id and uSkills[user_id] and uSkills[user_id][skill] then
         return uSkills[user_id][skill].Nivel
     end
+    return 0
 end
 
-AddEventHandler('vRP:playerSpawn', function(user_id,source,first_spawn)
+AddEventHandler('vRP:playerSpawn', function(user_id, source, first_spawn)
     if first_spawn then
         uSkills[user_id] = {}
-        exports.ghmattimysql:execute('SELECT PlayerSkills FROM vrp_users WHERE id = ?',{user_id},function(result)
+        exports.ghmattimysql:execute('SELECT PlayerSkills FROM vrp_users WHERE id = ?', {user_id}, function(result)
             if result[1].PlayerSkills then
                 uSkills[user_id] = json.decode(result[1].PlayerSkills) or {}
             end
@@ -52,10 +42,9 @@ AddEventHandler('vRP:playerSpawn', function(user_id,source,first_spawn)
     end
 end)
 
-AddEventHandler("vRP:playerLeave",function(user_id,source)
-    if uSkills[user_id] ~= nil then
-      exports.ghmattimysql:execute("UPDATE vrp_users SET PlayerSkills = ? WHERE id = ?",{json.encode(uSkills[user_id]),user_id})
-      uSkills[user_id] = nil
+AddEventHandler("vRP:playerLeave", function(user_id, source)
+    if uSkills[user_id] then
+        exports.ghmattimysql:execute("UPDATE vrp_users SET PlayerSkills = ? WHERE id = ?", {json.encode(uSkills[user_id]), user_id})
+        uSkills[user_id] = nil
     end
 end)
-
